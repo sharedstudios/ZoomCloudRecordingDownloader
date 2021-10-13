fd5ad1f6b965498c84d313698b4584adf4335fe5
yankang198@gmail.com <yankang198@gmail.com>
2021-10-13 09:11:39 -0400

    updated zoom api version, added google drive uploader

---
+++ googleDriveUploader.py
@@ -0,0 +1,60 @@
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+
+fl = open('api.txt', 'r')
+apis = fl.readlines()
+zoomRecordingsFolderId = apis[6].replace("\n", "")
+# to get folder id, run getFolderId function below and place it at line 7 of api.txt
+
+# place google drive api client_secrets.json under same directory
+
+gauth = GoogleAuth()
+gauth.LocalWebserverAuth()
+drive = GoogleDrive(gauth)
+
+
+
+def containsFile(title):
+    #check if drive contains file or not
+    query = f"title = '{title}'"
+    file_list = drive.ListFile({'q': query}).GetList()
+    return len(file_list) != 0
+
+def containsFolder(folderName):
+    folders = drive.ListFile({'q': "title='" + folderName + "' and trashed=false"}).GetList()
+    for folder in folders:
+        if folder['title'] == folderName:
+            return folder['id']
+    return False
+
+def createFolder(folderName, parentFolderId):
+    file_meta = {
+        'title': folderName,
+        'parents': [{"id": parentFolderId}],
+        'mimeType': 'application/vnd.google-apps.folder'
+    }
+
+    folder = drive.CreateFile(file_meta)
+    folder.Upload()
+    return containsFolder(folderName)
+
+def uploadFile(fileName, folderName):
+    if(containsFile(fileName)):
+        print(fileName + " exists in Google Drive, folder")
+        return
+    if not containsFolder(folderName):
+        print("Folder not exist, creating folder for " + folderName)
+        userFolderId = createFolder(folderName, zoomRecordingsFolderId)
+    print("Uploading file")
+    file2 = drive.CreateFile({'parents': [{'id': containsFolder(folderName)}]})
+    file2.SetContentFile(fileName)
+    file2.Upload()
+    print("Uploaded to Google Drive")
+
+# uploadFile('40138.jpeg', 'maria')
+# zoom recordings id = 1M6sMgwAx1UJgKB8OijRvgd8hTRMT5qoM
+
+def getFolderId():
+    file_list = drive.ListFile({'q': "'root' in parents and trashed=false"}).GetList()
+    for file1 in file_list:
+        print('title: %s, id: %s' % (file1['title'], file1['id']))
--- main.py
+++ main.py
@@ -2,149 +2,189 @@ import requests
 import webbrowser
 import json
 import os
-import urllib2
-import urllib
 from time import sleep
 import sys
+import datetime
+import googleDriveUploader
 
-#keeping our api key and api secret off the program file so it's secure
+# keeping our api key and api secret off the program file so it's secure
 fl = open('api.txt', 'r')
-apis = fl.readlines() 
+apis = fl.readlines()
 
-#print apis
+# print apis
 
-apiKey =  apis[0].replace("\n","")
-apiSec =  apis[1].replace("\n","")
-em = apis[2].replace("\n","")
-pas = apis[3].replace("\n","")
+apiKey = apis[0].replace("\n", "")
+apiSec = apis[1].replace("\n", "")
+em = apis[2].replace("\n", "")
+pas = apis[3].replace("\n", "")
+JWTtoken= apis[4].replace("\n", "")
 
-#this is defined to make sure no illegal character appear in potential filenames
+
+# this is defined to make sure no illegal character appear in potential filenames
 def correctFileName(filename):
     ret = filename.lower()
-    illegalFirstChars = [' ','.','_','-']
-    illegalChars = ['#','%','&','{','}','\\','<','>','*','?','/',' ','$','!',"'",'"',':','@','.']
+    illegalFirstChars = [' ', '.', '_', '-']
+    illegalChars = ['#', '%', '&', '{', '}', '\\', '<', '>', '*', '?', '/', ' ', '$', '!', "'", '"', ':', '@', '.']
     for index in range(len(filename)):
         if not filename[index] in illegalFirstChars:
             ret = filename[index:]
             break
 
     for char in illegalChars:
-        ret = ret.replace(char,"")
+        ret = ret.replace(char, "")
     return ret
 
-#had to implement sessions since zoom is annoying and forcing me to login to download their recordings
+
+# had to implement sessions since zoom is annoying and forcing me to login to download their recordings
 session_requests = requests.session()
 signin = "https://zoom.us/signin"
 values = {'email': em,
           'password': pas}
-result = session_requests.post(signin, data = values)
+result = session_requests.post(signin, data=values)
 
-#first set of parameters passed to api call
-userlistparam = {'api_key': apiKey, 'api_secret': apiSec , 'data_type': "JSON", 'page_size': 300}
+# first set of parameters passed to api call
+userlistparam = {'api_key': apiKey, 'api_secret': apiSec, 'data_type': "JSON", 'page_size': 300}
 
-#call to retrieve json object containing list of zoom users
-userlistjson = requests.post('https://api.zoom.us/v1/user/list', params=userlistparam).json()
-users =  userlistjson['users']
+headers = {
+    "authorization": "Bearer " + JWTtoken,
+}
+# call to retrieve json object containing list of zoom users
+userlistjson = requests.get('https://api.zoom.us/v2/users', headers=headers).json()
+users = userlistjson['users']
 
-#print len(users)
-#for i in range(10):
+# print len(users)
+# for i in range(10):
 
-#for loop to go through each user in the list
+# for loop to go through each user in the list
 for i in range(len(users)):
-    
-    #this retireves user that is currently being looked at and relevant info
+
+    # this retireves user that is currently being looked at and relevant info
     currUser = users[i]
-    #if user has any recordings, this string will be the name of created directory
+    # if user has any recordings, this string will be the name of created directory
     username = currUser['email'][:currUser['email'].find('@')]
-    #user id needed to pass to api call
+    # user id needed to pass to api call
     currID = currUser['id']
-    
-    print 'Checking User "%s" for New Recordings' % username 
-    #Waits 1 Second to Prevent Too Many API Calls at once
-    sleep(1)
-    
-    #second set of parameters passed to second api call
-    videolistparams = {'api_key': apiKey, 'api_secret': apiSec, 'data_type': "JSON", 'host_id': currID, 'page_size': 300}
-        
-    #call to retrieve json object cotaining list of user recordings
-    while True:
-        videolistjson = requests.post("https://api.zoom.us/v1/recording/list", params=videolistparams).json()
-        if "error" in videolistjson:
-            if videolistjson["error"]["code"] == 403:
-                print "Too Many API Calls at Once. Will Retry API Call in One Minute"
-                sleep(60)
-                continue
-            else:
-                sys.exit( "Unknown Error. Please run program again. If problem persists, contact justinp@sharedstudios.com")
-        
-        break
-    currUserMeetings = videolistjson['meetings'] 
-    
-    #if this user has any recordings, this block of code will download them
-    if (len(currUserMeetings) > 0):
-        print '%d Recordings found for User "%s"; Downloading the new recording MP4 Files' % (len(currUserMeetings), username)
-        #if folder for videos does not exist yet, creates a folder named after username
-        if not os.path.exists("./" + username):
-            os.makedirs("./" + username)
+    # earliest event date
+    firstDate = datetime.date(2014, 1, 1)
+    # current range of to date
+    currentTo = datetime.date.today()
+    # current range of from date
+    currentFrom = currentTo - datetime.timedelta(weeks=4)
 
-        #print username
-        #print " "
-        
-        #for loop iterates through a meeting object, and we specfically want to contruct the meeting name
-        #as well as iterate through recordings of a meeting
-        for j in range(len(currUserMeetings)):
-            currMeeting = currUserMeetings[j]
-            #print currMeeting
-            meetingName = currMeeting['topic'] + "-" + currMeeting['start_time']
-            meetingName = correctFileName(meetingName)
-            #print meetingName
+    # download the monthly recording until the first date of the events
+    while currentFrom >= firstDate:
 
-            
-            #a meeting might have multiple recordings, so this code block will iterate through those meetings
-            for k in range(len(currMeeting['recording_files'])):
-                
-                currRecording = currMeeting['recording_files'][k]
-                #some recording files are just audio, so we want to ensure we're only downloading the videos
-                #print currRecording
-                #uncomment the following if statement if you want to download only mp4 files
-#                 if currRecording['file_type'] != 'MP4':
-#                     continue
+        # convert date obj to string format
+        currentFromStr = currentFrom.strftime("%Y-%m-%d")
+        currentToStr = currentTo.strftime("%Y-%m-%d")
 
-                #we finally  get to the code where we start donwloading
-                filename = meetingName + '-' + str(k+1) + '.' + currRecording['file_type']
-                
-                #if recording file is already downloaded, this script won't download it again
-                #this allows us to call this script to download new files after downloading an inital batch
-                if not os.path.exists("./" + username + "/" + filename):
-                    link = currRecording['download_url']
-                    with open("./" + username + "/" + filename, "wb") as f:
-                        print "Downloading %s" % filename
-                        response = session_requests.get(link, stream=True)
-                        total_length = response.headers.get('content-length')
-                        
-                        if total_length is None: # no content length header
-                            f.write(response.content)
-                        else:
-                            dl = 0
-                            total_length = int(total_length)
-                            for data in response.iter_content(chunk_size=4096):
-                                dl += len(data)
-                                f.write(data)
-                                per = (100.0 * dl) / total_length
-                                done = int(50 * dl / total_length)
-                                sys.stdout.write("\r[%s%s]%f%%" % ('=' * done, ' ' * (50-done),per))   
-                                sys.stdout.flush()
-                        print "Finished Downloading %s" % filename
+        print('Checking User "%s" for recordings' % username + ' from ' + currentFromStr + ' to ' + currentToStr)
+        # Waits 1 Second to Prevent Too Many API Calls at once
+        sleep(1)
+
+
+        # second set of parameters passed to second api call
+        videolistparams = {'api_key': apiKey, 'api_secret': apiSec, 'data_type': "JSON", 'userId': currID, 'page_size': 300,
+                           'from': currentFromStr, 'to': currentToStr}
+
+        # call to retrieve json object cotaining list of user recordings
+        while True:
+            videolistjson = requests.get(f"https://api.zoom.us/v2/users/{currID}/recordings", headers=headers,
+                                         params=videolistparams).json()
+            if "error" in videolistjson:
+                if videolistjson["error"]["code"] == 403:
+                    print("Too Many API Calls at Once. Will Retry API Call in One Minute")
+                    sleep(60)
+                    continue
                 else:
-                    print "Already Downloaded %d.%d out of %d" % (j+1,k+1,len(currUserMeetings))
-                        
-                    #f.close()
+                    sys.exit(
+                        "Unknown Error. Please run program again. If problem persists, contact justinp@sharedstudios.com")
 
-                    #print filename
-            if j == (len(currUserMeetings) - 1):
-                print "Done Downloading User %s Recordings" % username
-                print
-                
-        #print username
+            break
+        currUserMeetings = videolistjson['meetings']
 
+        # if this user has any recordings, this block of code will download them
+        if (len(currUserMeetings) > 0):
+            print('%d Recordings found for User "%s"; Downloading the new recording MP4 Files' % (
+            len(currUserMeetings), username))
+            # if folder for videos does not exist yet, creates a folder named after username
+            #if not os.path.exists("./" + username):
+                # os.makedirs("./" + username)
 
+            # print username
+            # print " "
+
+            # for loop iterates through a meeting object, and we specfically want to contruct the meeting name
+            # as well as iterate through recordings of a meeting
+            for j in range(len(currUserMeetings)):
+                currMeeting = currUserMeetings[j]
+                # print currMeeting
+                meetingName = currMeeting['topic'] + "-" + currMeeting['start_time']
+                meetingName = correctFileName(meetingName)
+                # print meetingName
+
+                # a meeting might have multiple recordings, so this code block will iterate through those meetings
+                for k in range(len(currMeeting['recording_files'])):
+
+                    currRecording = currMeeting['recording_files'][k]
+                    # some recording files are just audio, so we want to ensure we're only downloading the videos
+                    # print currRecording
+
+                    # if currRecording['file_type'] != 'MP4':
+                    #     continue
+
+                    # we finally  get to the code where we start donwloading
+                    filename = meetingName + '-' + str(k + 1) + '.' + currRecording['file_type']
+
+                    # ignore uploading phase if it is on google drive
+
+                    if googleDriveUploader.containsFile(filename):
+                        print(f"Current file already exists in google drive -- {k+1}/{len(currMeeting['recording_files'])}")
+                        continue
+
+                    # if recording file is already downloaded, this script won't download it again
+                    # this allows us to call this script to download new files after downloading an inital batch
+                    if not os.path.exists(filename):  # to store locally, use ["./" + username + "/" + filename]
+                        link = currRecording['download_url']
+                        link += "?access_token="+JWTtoken
+                        with open(filename, "wb") as f:
+                            print("Downloading %s" % filename)
+                            response = session_requests.get(link, stream=True)
+                            # total_length = response.headers.get('file_size')
+                            total_length = currRecording['file_size']
+                            if total_length == 0:
+                                # f.write(response.content)
+                                continue
+                            else:
+                                print(currRecording)
+                                total_length = int(total_length)
+                                dl = 0
+                                for data in response.iter_content(chunk_size=4096):
+                                    dl += len(data)
+                                    f.write(data)
+                                    per = (100.0 * dl) / total_length
+                                    done = int(50 * dl / total_length)
+                                    sys.stdout.write("\r[%s%s]%f%%" % ('=' * done, ' ' * (50-done),per))
+                                    sys.stdout.flush()
+                                print(" Finished Downloading %s" % filename + f" {k+1} / {len(currMeeting['recording_files'])}")
+                                # upload file to google drive
+                                print(f.name)
+
+                                # comment the following 2 lines if you only want to download them locally
+                                googleDriveUploader.uploadFile(f.name, username)
+                                # delete local file
+                                os.remove(f.name)
+                    else:
+                        print("Already Downloaded %d.%d out of %d" % (j + 1, k + 1, len(currUserMeetings)))
+
+                        # f.close()
+
+                        # print filename
+                if j == (len(currUserMeetings) - 1):
+                    print("Done Downloading User %s Recordings" % username + ' from ' + currentFromStr + ' to ' + currentToStr)
+                    print
+
+        currentTo = currentFrom
+        currentFrom -= datetime.timedelta(weeks=4)
+
+        # print username
